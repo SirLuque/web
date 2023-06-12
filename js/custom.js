49b8559412c376309c62d1bdce4d2c9d9e5874e8
SirLuque <79418082+SirLuque@users.noreply.github.com>
2023-06-12 08:42:48 -0300

    Commit 001

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+	$("#navigationBar").children().each((index, element) => {
+		textos = ["Principal", "Nosotros", "Portafolio", "Contacto"];
+
+		var newSpan = $("<span></span>");
+		$(newSpan).css("margin-left", "auto");
+		$(newSpan).css("padding-right", "15px");
+		$(newSpan).html(textos[index]);
+		
+		$(element).html(newSpan);
+		$(element).css("display", "flex");
+		$(element).css("flexDirection", "revert");
+		$(element).css("alignItems", "center");
+	})
+})
+
